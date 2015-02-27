bbe232dafec774d5d08e2cc268247db1e715e2a5
Chuck Rose <chuckr@adobe.com>
2015-02-27 11:10:07 -0800

    Set up a shell project

---
+++ projects/xcode/anthill/anthill/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  anthill
+//
+//  Created by Chuck Rose on 2/27/15.
+//  Copyright (c) 2015 cfr3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ projects/xcode/anthill/anthill/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  anthill
+//
+//  Created by Chuck Rose on 2/27/15.
+//  Copyright (c) 2015 cfr3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
