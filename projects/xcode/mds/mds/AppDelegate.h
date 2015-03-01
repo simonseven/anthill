545fb02a5d3a61a0e5e75efe2d86945fcfe7d9fa
Chuck Rose <chuckr@adobe.com>
2015-03-01 13:25:09 -0800

    Adding shell app

---
+++ projects/xcode/mds/mds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mds
+//
+//  Created by Chuck Rose on 3/1/15.
+//  Copyright (c) 2015 cfr3. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ projects/xcode/mds/mds/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  mds
+//
+//  Created by Chuck Rose on 3/1/15.
+//  Copyright (c) 2015 cfr3. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
